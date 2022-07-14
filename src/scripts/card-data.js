84314ca51aca3016696534562df46d826a7e8247
Boris Gaidukevich <Borishahn@yandex.ru>
2022-07-15 00:48:31 +0300

    OOP 2.1.1

---
+++ src/scripts/card-data.js
@@ -0,0 +1,35 @@
+import bmwR9T from '../imges/fabio-spinelli-cPpmFa1OiGU-unsplash.jpg';
+import reims from '../imges/petr-stradal-VwgQxs1-xLM-unsplash.jpg';
+import yamahaR6 from '../imges/max-itin-xlhl7rI2M4I-unsplash.jpg';
+import farer from '../imges/waterfalls.jpg';
+import utah from '../imges/utah.jpg';
+import lofoten from '../imges/lofoten.jpg';
+
+const initialCards = [
+    {
+      name: 'БМВ R9T',
+      link: bmwR9T
+    },
+    {
+      name: 'Реймс',
+      link: reims
+    },
+    {
+      name: 'Ямаха R6',
+      link: yamahaR6
+    },
+    {
+      name: 'Фареры',
+      link: farer
+    },
+    {
+      name: 'Юта, США',
+      link: utah
+    },
+    {
+      name: 'Лофотены',
+      link: lofoten
+    }
+  ];
+
+  export default initialCards;
